b2bda5a64e7c2c7a09cac1acd8407802a9016189
Zheng Wei Quah <quahzhengwei@zhengs-macbook.local>
2016-07-25 12:22:22 +0800

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Zheng Wei Quah on 25/7/16.
+//  Copyright © 2016 Zheng Wei Quah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
